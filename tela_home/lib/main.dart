2564bab0a040b8587df9c7995db1d965c6d4838a
R234H <jkttiu@gmail.com>
2023-06-21 17:53:31 -0300

    tela login

--- tela_home/lib/main.dart
+++ tela_home/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tela_inicial/home_screen.dart';
+//import 'package:tela_inicial/home_screen.dart';
+import 'package:tela_inicial/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(nome: 'Hiel'),
+      home: const LoginPage(),
     );
   }
 }
